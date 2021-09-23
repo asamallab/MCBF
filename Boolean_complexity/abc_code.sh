b23c6faec6935455c47448b7e22c7cbe55f26767
asamallab <75881572+asamallab@users.noreply.github.com>
2021-09-23 20:27:29 +0530

    Update abc_code.sh

--- Boolean_complexity/abc_code.sh
+++ Boolean_complexity/abc_code.sh
@@ -2,4 +2,3 @@
 ./abc read_eqn input_BF/repBF_full_dnf032.eqn; sop; write output_BF/fact_repBF_full_dnf032.eqn
 ./abc read_eqn input_BF/repBF_qm_cnf032.eqn; sop; write output_BF/fact_repBF_qm_cnf032.eqn
 ./abc read_eqn input_BF/repBF_qm_dnf032.eqn; sop; write output_BF/fact_repBF_qm_dnf032.eqn
-
