d2007bc0f60d4b8c97012eb61bb35d9b5ffe2c34
asamallab <75881572+asamallab@users.noreply.github.com>
2021-09-26 19:01:32 +0530

    Update abc_code.sh

--- Boolean_complexity/abc_code.sh
+++ Boolean_complexity/abc_code.sh
@@ -1,3 +1,13 @@
+"""
+====================================================================
+Written by: Ajay Subbaroyan
+Program to compute the Boolean complexity
+Reference: "Minimum complexity drives regulatory logic in Boolean models of living systems".
+Ajay Subbaroyan, Olivier C. Martin, Areejit Samal, bioRxiv 2021.09.20.461164; 
+doi: https://doi.org/10.1101/2021.09.20.461164
+====================================================================
+"""
+
 ./abc read_eqn input_BF/repBF_full_cnf032.eqn; sop; write output_BF/fact_repBF_full_cnf032.eqn
 ./abc read_eqn input_BF/repBF_full_dnf032.eqn; sop; write output_BF/fact_repBF_full_dnf032.eqn
 ./abc read_eqn input_BF/repBF_qm_cnf032.eqn; sop; write output_BF/fact_repBF_qm_cnf032.eqn
