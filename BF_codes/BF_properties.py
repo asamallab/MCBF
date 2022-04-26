16aead77de3fd53f5cdea1740812830d748b92cf
Areejit Samal Laboratory <75881572+asamallab@users.noreply.github.com>
2022-04-26 14:51:42 +0530

    Update BF_properties.py

--- BF_codes/BF_properties.py
+++ BF_codes/BF_properties.py
@@ -3,7 +3,7 @@
 Program to obtain perform operations on Boolean functions and get their
 various properties
 Written by: Ajay Subbaroyan
-Ajay Subbaroyan, Olivier C Martin, Areejit Samal, 
+Ajay Subbaroyan, Olivier C Martin, Areejit Samal,
 Minimum complexity drives regulatory logic in Boolean models of living systems, 
 PNAS Nexus, Volume 1, Issue 1, March 2022, pgac017, 
 https://doi.org/10.1093/pnasnexus/pgac017
